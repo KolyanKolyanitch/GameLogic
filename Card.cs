c677ea64c536e430bca3f7e7c125948d7306a269
KolyanKolyanitch <niksolay@gmail.com>
2015-02-15 15:59:48 +0100

    Finished card descr

--- Card.cs
+++ Card.cs
@@ -10,19 +10,51 @@ namespace GameLogic
     {
         public string Name;
         public int Price;
-        public int Damage;
-        public int Health;
         public string DescriptionEffectCard;
         public string ArtisticDescription;
+        public string CardType;
+        public string Rarity;
 
-        public Card(int Price, int Damage, int Health, string Name, string DescriptionEffectCard, string ArtisticDescription)
+        public struct EffectDescription //применимо к зданиям
+        {
+            public int changeAllHealth;
+            public int changeAllDamage;
+            public int changeTargetHealth;
+            public int changeTargetDamage;
+            public bool giveShield;
+            public bool giveTwoAttacks;
+            public bool giveStealth;
+            public bool killStealth;
+            public bool giveSilens;
+
+            public string callCreature;
+        }
+        public struct CreatureDescription
+        {
+            public int damage;
+            public int health;
+            public EffectDescription effects;
+        }
+        public struct SpellDescription
+        {
+            public bool isAOE;
+            public EffectDescription effects;
+        }
+        public struct AmmunitionDescr
+        {
+            public bool isWeapon;
+            public int durability;
+            public int defence;
+        }
+
+        public Card(int Price, string Name, string DescriptionEffectCard, string ArtisticDescription)
         {
             this.Price = Price;
-            this.Damage = Damage;
-            this.Health = Health;
             this.Name = Name;
             this.DescriptionEffectCard = DescriptionEffectCard;
             this.ArtisticDescription = ArtisticDescription;
         }
+
+
     }
 }
