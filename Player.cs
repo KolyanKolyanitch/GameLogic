55d70910cbfad2dd8cdb9fa8dc516b9149905c5c
KolyanKolyanitch <niksolay@gmail.com>
2015-02-15 15:13:01 +0100

    start

---
+++ Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class Player
+    {
+        string Name;
+        int[] cards;
+        TcpClient client;
+        StreamReader reader;
+        StreamWriter writer;
+    }
+}
